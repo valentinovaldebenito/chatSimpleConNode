5d62e133029d2a21c1c25c59f86d0ab91b66ae54
valentinovaldebenito <valentinovaldebenito.dev@gmail.com>
2024-06-20 20:54:32 -0300

    Fix app.js, index.html | Se eliminaron los console.logs y codigo comentado.

--- app.js
+++ app.js
@@ -8,20 +8,13 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 
 io.on('connection', (socket) => {
-    /* console.log('Un usuario se ha conectado') */
 
-
-    /* socket.on('chat', (msg) => {
-        console.log('Mensaje: '+msg)
-    }) */
-
-        socket.on('chat', (msg) => {
-            io.emit('chat', msg)
-        })
+    socket.on('chat', (msg) => {
+        io.emit('chat', msg)
+    })
 })
 
 app.get("/", (req, res) => {
-  /* res.send(`<h1>Aplicacion de Chat</h1>`) */
   res.sendFile(`${__dirname}/client/index.html`);
 });
 
